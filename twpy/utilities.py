e8a6860ec3ab05938a5c52198f2366341f4cf119
Serhat Teker <serhat.teker@gmail.com>
2020-05-01 17:51:44 +0300

    Add raise error for get_api_token

--- twpy/utilities.py
+++ twpy/utilities.py
@@ -1,5 +1,12 @@
 import os
 
 
-def get_token(key='API_TOKEN'):
-    return os.getenv(key, default=None)
+def get_api_token(key='API_TOKEN'):
+    """Simple retrieval function.
+    Returns API_TOKEN or raises OSError."""
+    api_token = os.getenv(key, default=None)
+
+    if api_token is None:
+        raise OSError("API_TOKEN environment is not set.")
+
+    return api_token
